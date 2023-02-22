b54b24a4b5b9840686ec5a8cd7d9f08a9224f864
Raushan Kumar Gupta <raushan.gupta@zemosolabs.com>
2023-02-22 22:07:17 +0530

    Add: Method for encoding

--- first_git_repo/Data-Encryption-System/DataEncryptionSystem.java
+++ first_git_repo/Data-Encryption-System/DataEncryptionSystem.java
@@ -1,4 +1,44 @@
+import java.util.Scanner;
 public class DataEncryptionSystem{
+
+
+    // Method to encode the data
+    public static StringBuilder encodeMessage(String message) {
+        // To store the encoded message
+        String encMessage = new String();
+
+        for (int i = 0; i < message.length(); i++) {
+            // Check for uppercase
+            char ch = message.charAt(i);
+            if (checkUpperCase(ch)) {
+                encMessage += ((char) ('A' + (25 - (ch - 'A'))));
+            }
+            // Check for lowercase
+            else if (checkLowerCase(ch)) {
+                encMessage += ((char) ('a' + (25 - (ch - 'a'))));
+            } else if (Character.isDigit(ch)) {
+                // Converting character to its integer value
+                int digit = ch - '0';
+                for (int j = 0; j < (digit % 2); j++)
+                    encMessage += ((char) digit % 2);
+            }
+            // For special character
+            else {
+                int digit = ch - '0';
+                // Shifting the special char by (digit/33)
+                char newChar = (char) (ch + (digit / 33)); // Dividing by 33 because, no of special char is 33.
+                encMessage += newChar;
+            }
+        }
+        // To store the reverse of encoded Message
+        StringBuilder strBuilder = new StringBuilder();
+        strBuilder.append(encMessage);
+
+        // Reversing the encMessage for adding more complexity to guess
+        StringBuilder encMessageReverse = strBuilder.reverse();
+
+        return encMessageReverse;
+    }
 
     public static void main(String[] args) {
         System.out.println("<====Welcome to the Message Encode System ====>");
