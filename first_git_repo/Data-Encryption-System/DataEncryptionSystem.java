673dc33d357be5363c802ab12ad5e5b40efb6cc3
Raushan Kumar Gupta <raushan.gupta@zemosolabs.com>
2023-02-22 22:04:57 +0530

    Add: Main method & taking input

--- first_git_repo/Data-Encryption-System/DataEncryptionSystem.java
+++ first_git_repo/Data-Encryption-System/DataEncryptionSystem.java
@@ -1,6 +1,11 @@
 public class DataEncryptionSystem{
 
     public static void main(String[] args) {
-        
+        System.out.println("<====Welcome to the Message Encode System ====>");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your message to encode: ");
+        // Taking data input from the user
+        String message = sc.nextLine();
     }
 }
