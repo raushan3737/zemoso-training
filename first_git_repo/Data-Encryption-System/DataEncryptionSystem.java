d4ee06fd93b7c8cb4cf1daff386283ce2bb0d9c3
Raushan Kumar Gupta <raushan.gupta@zemosolabs.com>
2023-02-23 13:25:23 +0530

    Modified: Optimising the space complexity by just by changing the data type String to StringBuilder for the input message

--- first_git_repo/Data-Encryption-System/DataEncryptionSystem.java
+++ first_git_repo/Data-Encryption-System/DataEncryptionSystem.java
@@ -55,43 +55,39 @@ public class DataEncryptionSystem{
                     Space Complexity:O(N) , Where, N is the length of the input string/message
 
     */
-    public static StringBuilder encodeMessage(String message) // 
+    public static StringBuilder encodeMessage(StringBuilder message) 
      {
         // To store the encoded message
-        String encMessage = new String();
+        StringBuilder encMessage = new StringBuilder();
 
         for (int i = 0; i < message.length(); i++) {
             // Check for uppercase
             char ch = message.charAt(i);
             if (checkUpperCase(ch)) {
-                encMessage += ((char) ('A' + (25 - (ch - 'A'))));
+                encMessage.append( ((char) ('A' + (25 - (ch - 'A')))));
             }
             // Check for lowercase
             else if (checkLowerCase(ch)) {
-                encMessage += ((char) ('a' + (25 - (ch - 'a'))));
+                encMessage.append(  ((char) ('a' + (25 - (ch - 'a')))));
             } else if (Character.isDigit(ch)) {
                 // Converting character to its integer value
                 int digit = ch - '0';
                 for (int j = 0; j < (digit % 2); j++)
-                    encMessage += ((char) digit % 2);
+                encMessage.append(  ((char) digit % 2));
             }
             // For special character
             else {
                 int digit = ch - '0';
                 // Shifting the special char by (digit/33)
                 char newChar = (char) (ch + (digit / 33)); // Dividing by 33 because, no of special char is 33.
-                encMessage += newChar;
+                encMessage.append(  newChar);
             }
         }
 
-        // To store the reverse of encoded Message
-        StringBuilder strBuilder = new StringBuilder();
-        strBuilder.append(encMessage);
-
         // Reversing the encMessage for adding more complexity to guess
-        StringBuilder encMessageReverse = strBuilder.reverse();
+        encMessage.reverse();
 
-        return encMessageReverse;
+        return encMessage;
     }
 
     public static void main(String[] args) {
@@ -102,9 +98,11 @@ public class DataEncryptionSystem{
         System.out.println("Enter your message to encode: ");
         // Taking data input from the user
         String message = sc.nextLine();
+        StringBuilder userMessage = new StringBuilder(message) ; 
+
 
         // Invoking the encodeMessage method to encode the message/data
-        StringBuilder encodeMsg= encodeMessage(message); 
+        StringBuilder encodeMsg= encodeMessage(userMessage); 
 		System.out.println("Encoded message: "+ encodeMsg);
 
 
